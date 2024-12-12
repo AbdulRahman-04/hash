98c92a8bec14c1c1effcd074c073f14a8ec47251
Abdul Rahman <Abdulrahman.81869@gmail.com>
2024-12-12 06:47:18 +0000

    bcrypy

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import bcrypt from "bcryptjs"
 
+
+// hasihing a password:
 async function hashPassword(params) {
-    
+     
 
     try {
 
@@ -17,4 +19,31 @@ async function hashPassword(params) {
 
 }
 
-hashPassword()
+hashPassword();
+
+
+
+
+// verifying the password using hash value:
+
+async function verifyPass() {
+    
+    try {
+        let hashValue = "$2a$10$gQq9m5o7nwAurPFvwqotJeCTAh3L/vnKlTEwF8h9qd4qQQveKOGlG"
+        let myPassword = "rahman004";
+        let verify = await bcrypt.compare(myPassword, hashValue);
+        console.log(verify);
+        
+        if(verify){
+            console.log('Login successful');
+            
+        } else {
+            console.log('invalid password!');
+            
+        }
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+verifyPass()
